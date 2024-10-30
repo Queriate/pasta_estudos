116f9834f61eb2341896009c5dac0edbabffe108
queriate <queriatejobs@gmail.com>
2024-10-30 16:07:14 -0300

    novos tipos

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 
 let x: number = 10;
 x = 16;
-console.log(x);
+// console.log(x);
 
 // inteferencia x annotation
 
@@ -10,4 +10,31 @@ let y = 12 //inferencia
 
 let z: number = 13
 
-console.log(y,z);
+// console.log(y,z);
+
+let mensagem: string = "Olá mundo! Espero que esteja bem";
+// console.log(mensagem);
+
+
+//tipos básicos
+let fistName: string = "Queriate"
+let age: number = 27
+const isAdmin: boolean = true
+
+// String != string
+
+console.log(typeof fistName);
+
+let firstName: string = "João"
+
+console.log(firstName);
+
+// object
+
+const myNumbers: number[] = [1,2,3]
+
+console.log(myNumbers);
+console.log(myNumbers.length);
+console.log(fistName.toUpperCase());
+
+//tuplas
