21d4bc6b7b4c9c21f615dceecbf88ce3fcfe7b27
queriate <queriatejobs@gmail.com>
2024-10-30 16:49:28 -0300

    ptc

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,25 +1,7 @@
 "use strict";
-//tipagens string, boolean, number...
-let x = 10;
-x = 16;
-// console.log(x);
-// inteferencia x annotation
-let y = 12; //inferencia 
-let z = 13;
-// console.log(y,z);
-let mensagem = "Olá mundo! Espero que esteja bem";
-// console.log(mensagem);
-//tipos básicos
-let fistName = "Queriate";
-let age = 27;
-const isAdmin = true;
-// String != string
-console.log(typeof fistName);
-let firstName = "João";
-console.log(firstName);
-// object
-const myNumbers = [1, 2, 3];
-console.log(myNumbers);
-console.log(myNumbers.length);
-console.log(fistName.toUpperCase());
-//tuplas
+const user = "User:";
+const admin = "Queriate";
+const ageId = "Idade:";
+const age = 27;
+console.log(user, admin);
+console.log(ageId, age);
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,7 @@
-//tipagens string, boolean, number...
+const user: string = "User:";
+const admin: string = "Queriate";
+const ageId: string = "Idade:";
+const age: number = 27
 
-let x: number = 10;
-x = 16;
-// console.log(x);
-
-// inteferencia x annotation
-
-let y = 12 //inferencia 
-
-let z: number = 13
-
-// console.log(y,z);
-
-let mensagem: string = "Olá mundo! Espero que esteja bem";
-// console.log(mensagem);
-
-
-//tipos básicos
-let fistName: string = "Queriate"
-let age: number = 27
-const isAdmin: boolean = true
-
-// String != string
-
-console.log(typeof fistName);
-
-let firstName: string = "João"
-
-console.log(firstName);
-
-// object
-
-const myNumbers: number[] = [1,2,3]
-
-console.log(myNumbers);
-console.log(myNumbers.length);
-console.log(fistName.toUpperCase());
-
-//tuplas
+console.log(user, admin);
+console.log(ageId, age);
